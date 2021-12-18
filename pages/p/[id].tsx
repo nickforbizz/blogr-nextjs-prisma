5c7bea9bc2389d2f2dbbee6ec25a16eb0aa9774b
nickforbizz <nwaruingi@kemri-wellcome.org>
2021-12-18 07:29:40 +0300

    auth with github

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,20 +1,21 @@
 import React from "react"
 import { GetServerSideProps } from "next"
 import ReactMarkdown from "react-markdown"
+import prisma from '../../lib/prisma';
 import Layout from "../../components/Layout"
 import { PostProps } from "../../components/Post"
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const post = {
-    id: 1,
-    title: "Prisma is the perfect ORM for Next.js",
-    content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-    published: false,
-    author: {
-      name: "Nikolas Burk",
-      email: "burk@prisma.io",
+  const post = await prisma.post.findUnique({
+    where: {
+      id: Number(params?.id) || -1,
     },
-  }
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
   return {
     props: post,
   }
